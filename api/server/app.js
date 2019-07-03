210c3ca2941a52ff610bc0a58516411bca81909c
Bright Emah <bemah2020@gmail.com>
2019-07-03 19:59:59 +0100

    feat(create express server)

---
+++ api/server/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import debug from 'debug';
+// import router from './routes';
+import morgan from 'morgan';
+
+const Debug = debug('http');
+
+const app = express();
+
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use('/api/v1', router);
+
+app.get('/api/v1/', (req,res)=> {
+    res.status(200).json({
+        status: 'success',
+        message: 'Welcome to Wayfarer'
+    })
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> {
+    Debug(`App is listening at port ${port}`)
+});
+
+export default app;
